e248ed0feac29557f607c9fd87842c0ad2e91b48
Aaron Hodel <aaronhodel@gmail.com>
2016-08-20 22:53:24 +0200

    More Events

--- src/ch/aiko/pokemon/basic/PokemonEvents.java
+++ src/ch/aiko/pokemon/basic/PokemonEvents.java
@@ -2,18 +2,22 @@ package ch.aiko.pokemon.basic;
 
 import java.net.Socket;
 
+import ch.aiko.engine.graphics.Window;
 import ch.aiko.modloader.GameEvent;
 import ch.aiko.pokemon.entity.player.PlayerMenu;
+import ch.aiko.pokemon.pokemons.TeamPokemon;
 import ch.aiko.pokemon.server.ServerPlayer;
 
 public class PokemonEvents {
-	
+
 	public static final String PLAYER_MENU_OPEN_EVENT = "playeropenmenu";
 	public static final String SERVER_START = "serverstart";
 	public static final String CLIENT_START = "clientstart";
 	public static final String PLAYER_CONNECTED = "playerconnect";
 	public static final String STRING_RECEIVED = "receiveddata";
-	//TODO more events (for mods)
+	public static final String POKEMON_DEFEATED = "pokemondefeat";
+	public static final String WINDOW_CLOSING = "windowclosing";
+	// TODO more events (for mods)
 
 	public static class PlayerOpenMenuEvent extends GameEvent {
 		public String eventName() {
@@ -26,13 +30,13 @@ public class PokemonEvents {
 			menu = m;
 		}
 	}
-	
+
 	public static class ServerStartEvent extends GameEvent {
 		public String eventName() {
 			return SERVER_START;
 		}
 	}
-	
+
 	public static class ClientStartEvent extends GameEvent {
 		public String eventName() {
 			return CLIENT_START;
@@ -43,16 +47,48 @@ public class PokemonEvents {
 		public String eventName() {
 			return PLAYER_CONNECTED;
 		}
+
 		public ServerPlayer player;
+
 		public PlayerConnectedEvent(ServerPlayer p) {}
 	}
-	
+
 	public static class StringReceivedEvent extends GameEvent {
 		public String eventName() {
 			return STRING_RECEIVED;
 		}
+
 		public String received;
 		public Socket socket;
-		public StringReceivedEvent(String re, Socket s) {received = re;socket = s;}
+
+		public StringReceivedEvent(String re, Socket s) {
+			received = re;
+			socket = s;
+		}
+	}
+
+	public static class PokemonDefeatEvent extends GameEvent {
+		public String eventName() {
+			return POKEMON_DEFEATED;
+		}
+
+		public TeamPokemon defeated, other;
+
+		public PokemonDefeatEvent(TeamPokemon pok1, TeamPokemon pok2) {
+			defeated = pok1;
+			other = pok2;
+		}
+	}
+	
+	public static class WindowClosingEvent extends GameEvent {
+		public String eventName() {
+			return WINDOW_CLOSING;
+		}
+
+		public Window window;
+		
+		public WindowClosingEvent(Window window) {
+			this.window = window;
+		}
 	}
 }
